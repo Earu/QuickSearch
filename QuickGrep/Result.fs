8d1591e4c6237734f78a708efd529c82b634bfcf
Earu <reno.reboursier@gmail.com>
2018-05-18 20:33:50 +0200

    Initial commit

---
+++ QuickGrep/Result.fs
@@ -0,0 +1,8 @@
+﻿namespace QuickSearch.Result
+
+open System
+
+type Result(line,line_number,path) =
+    member self.line: String = line
+    member self.line_number: int = line_number
+    member self.path: String = path
